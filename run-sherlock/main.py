5ddb0c34ffec8b0fcebd9ec3a55dc9827afadd92
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2023-09-29 22:49:38 -0700

    Add cors stuff

--- run-sherlock/main.py
+++ run-sherlock/main.py
@@ -11,10 +11,22 @@ def endpoint(request):
         Response object using
         `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
     """
+    if request.method == 'OPTIONS':
+        headers = {
+            'Access-Control-Allow-Origin': '*',  # Allow your function to be called from any domain
+            'Access-Control-Allow-Methods': '*',  # Allow all HTTP methods
+            'Access-Control-Allow-Headers': 'Content-Type', 
+            'Access-Control-Max-Age': '3600',
+        }
+        return ('', 204, headers)
+
+    headers = {
+        'Access-Control-Allow-Origin': '*',
+    }
 
     request_json = request.get_json()
     if 'username' in request_json:
         sherlock_json = sherlock.req_json(request_json['username'])
-        return sherlock_json
+        return sherlock_json, 200, headers
     else:
-        return 'Invalid username', 400
+        return 'Invalid username', 400, headers
