b3359ce302fa97e41caed756ac12a733e0c63ab1
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2023-09-30 16:47:50 -0700

    Run sherlock search in separate thread and make SSE endpoint for search results

--- run-sherlock/main.py
+++ run-sherlock/main.py
@@ -1,9 +1,16 @@
 import sys
 sys.path.append('sherlock/sherlock')
 import sherlock
-from fastapi import FastAPI
+from res_queue import QueryNotifyQueue
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from sse_starlette.sse import EventSourceResponse
+import asyncio
+from _thread import start_new_thread
 from pydantic import BaseModel
+
+STREAM_DELAY = 0.1
+query_notify = QueryNotifyQueue()
 
 class SherlockQuery(BaseModel):
     username: str
@@ -21,5 +28,18 @@ app.add_middleware(
 
 @app.post("/")
 async def endpoint(item: SherlockQuery):
-    sherlock_json = sherlock.req_json(item.username, item.extra)
-    return sherlock_json
+    start_new_thread(sherlock.req_json, (item.username, item.extra, query_notify))
+    return item
+
+@app.get("/stream")
+async def stream(request: Request):
+    async def event_generator():
+        while True:
+            if await request.is_disconnected():
+                break
+
+            if not query_notify.queueEmpty():
+                yield str(query_notify.queuePop())
+            await asyncio.sleep(STREAM_DELAY)
+    
+    return EventSourceResponse(event_generator())
